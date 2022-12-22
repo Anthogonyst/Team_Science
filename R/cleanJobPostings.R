e273a07860f9f90e6e674379764db4766ea42f12
anthogonyst <anthogonyst@gmail.com>
2022-12-22 17:19:16 -0500

    Expose regex to wrapper

--- R/cleanJobPostings.R
+++ R/cleanJobPostings.R
@@ -44,6 +44,9 @@ library(jsonlite)
 #' Cleans a mthml file and produces a minified html and json file in the same folder, unless optionally specified
 #' @param fp The filepath to a mhtml file
 #' @param out The filepath to produce the output, default is the same folder
+#' @param lhs A html regex for subsetting raw mhtml files
+#' @param rhs A html regex for subsetting raw mhtml files
+#' @param combined A html regex for cleaning the result
 #' @examples
 #' \dontrun{
 #' file = "data/indeed/1Data Scientist Jobs, Employment in Austin, TX _ Indeed.com.mhtml"
@@ -57,17 +60,14 @@ library(jsonlite)
 #' }
 #' @author Anthogonyst
 #' @export
-CleanMHtml <- function(fp, out = gsub("\\..*$", "", fp)) {
+CleanMHtml <- function(fp, out = gsub("\\..*$", "", fp), lhs, rhs, combined) {
   raw = readLines(fp)
-  lhsHtml = '<div id="jobsearch-ViewjobPaneWrapper"'
-  rhsHTML = 'Return to Search Result</button></div>'
   
   body = raw %>%
     .FixMHtml(.) %>% 
       gsub('3D\\"', '"', .) %>%
         seqinr::c2s(.) %>%
-          gsub(paste0(".*", lhsHtml, "(.+)", rhsHTML, ".*"), 
-               "<div\\1</button></div>", .)
+          gsub(paste0(".*", lhs, "(.+)", rhs, ".*"), combined, .)
   
   writeLines(body, paste0(out, "-mini.html"))
   
@@ -78,9 +78,27 @@ CleanMHtml <- function(fp, out = gsub("\\..*$", "", fp)) {
   return(c(paste0(out, "-mini.html"), paste0(out, "-mini.json")))
 }
 
+.CleanSavedLinkedinJobs <- function(fp, out = gsub("\\..*$", "", fp)) {
+  
+  lhsHtml = '<div class="jobs-box'
+  rhsHtml = '<footer class="artdeco-card__actions">'
+  combinedHtml = '<div class="jobs-box\\1</div>'
+  
+  CleanMHtml(fp, out, lhsHtml, rhsHtml, combinedHtml) 
+}
+
+.CleanLinkedinPosting <- function(fp, out = gsub("\\..*$", "", fp)) {
+  lhsHtml = '<div id="jobsearch-ViewjobPaneWrapper"'
+  rhsHtml = 'Return to Search Result</button></div>'
+  combinedHtml = '<div\\1</button></div>'
+  
+  CleanMHtml(fp, out, lhsHtml, rhsHtml, combinedHtml) 
+}
+
 #' Cleans the mthml files inside the specified /data/ folder
 #' @param dataFolder The filepath holding all of your mhtml files
 #' @param outFolder The filepath to produce the output, default is the same folder
+#' @param mhtmlFUN A closure with the mhtml cleaning procedure
 #' @examples
 #' \dontrun{
 #' folder = "data/linkedin/"
@@ -91,12 +109,12 @@ CleanMHtml <- function(fp, out = gsub("\\..*$", "", fp)) {
 #' }
 #' @author Anthogonyst
 #' @export
-CleanerRunner <- function(dataFolder = "data/indeed/", outFolder = "data/cleaned_indeed/") {
+CleanerRunner <- function(dataFolder = "data/indeed/", outFolder = "data/cleaned_indeed/", mhtmlFUN = .CleanLinkedinPosting) {
   if (! dir.exists(outFolder))
     dir.create(outFolder)
   
   list.files(dataFolder, pattern = "\\.mhtml$", full.names = TRUE) %>%
     { list(., paste0(outFolder, "/", gsub("\\.mhtml", "", basename(.)))) } %>%
-      { mapply(.[[1]], .[[2]], FUN = CleanMHtml, USE.NAMES = FALSE) }
+      { mapply(.[[1]], .[[2]], FUN = mhtmlFUN, USE.NAMES = FALSE) }
 }
   
